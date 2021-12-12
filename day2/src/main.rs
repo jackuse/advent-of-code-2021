dc147df5c27dcb1599024bbe6dd40e785120e357
Etienne Grandier-Vazeille <etienne.grandiervazeille@gmail.com>
2021-12-12 13:43:19 +0100

    feat(day2): add part 1 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,23 +1,52 @@
 use shared::read_file;
 
 fn main() {
-    println!("Hello, world!");
-    part1();
+    let filename = "src/input.txt";
+    let commands = read_file(filename);
+
+    // commands.iter().for_each(|c| println!("{}", *c));
+
+    let res = part1(&commands);
+    println!("PART 1 : {}", res);
 }
 
-fn part1() {}
+fn part1(commands: &Vec<String>) -> i32 {
+    let mut h_pos = 0;
+    let mut depth_pos = 0;
+
+    commands.iter().for_each(|command| {
+        let tmp = command.split(" ").collect::<Vec<&str>>();
+        let action = tmp[0];
+        let power = tmp[1].parse::<i32>().unwrap();
+
+        if action == "forward" {
+            h_pos += power;
+        }
+        if action == "down" {
+            depth_pos += power;
+        }
+        if action == "up" {
+            depth_pos -= power;
+        }
+    });
+
+    h_pos * depth_pos
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn part1() {
-        assert_eq!(2 + 2, 4);
+    fn should_pass_part1_demo() {
+        let filename = "src/demo.txt";
+        let res = part1(&read_file(filename));
+
+        assert_eq!(res, 150);
     }
 
     #[test]
-    fn part2() {
+    fn should_pass_part2_demo() {
         assert_eq!(2 + 2, 4);
     }
 }
