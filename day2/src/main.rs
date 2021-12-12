4b028263742edefe0c33e1a32cb793e6e1b36f88
Etienne Grandier-Vazeille <etienne.grandiervazeille@gmail.com>
2021-12-12 13:48:44 +0100

    feat(day2): add part 2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,6 +8,9 @@ fn main() {
 
     let res = part1(&commands);
     println!("PART 1 : {}", res);
+
+    let res = part2(&commands);
+    println!("PART 2 : {}", res);
 }
 
 fn part1(commands: &Vec<String>) -> i32 {
@@ -33,6 +36,31 @@ fn part1(commands: &Vec<String>) -> i32 {
     h_pos * depth_pos
 }
 
+fn part2(commands: &Vec<String>) -> i32 {
+    let mut h_pos = 0;
+    let mut depth_pos = 0;
+    let mut aim = 0;
+
+    commands.iter().for_each(|command| {
+        let tmp = command.split(" ").collect::<Vec<&str>>();
+        let action = tmp[0];
+        let power = tmp[1].parse::<i32>().unwrap();
+
+        if action == "forward" {
+            h_pos += power;
+            depth_pos += aim * power;
+        }
+        if action == "down" {
+            aim += power;
+        }
+        if action == "up" {
+            aim -= power;
+        }
+    });
+
+    h_pos * depth_pos
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,6 +75,9 @@ mod tests {
 
     #[test]
     fn should_pass_part2_demo() {
-        assert_eq!(2 + 2, 4);
+        let filename = "src/demo.txt";
+        let res = part2(&read_file(filename));
+
+        assert_eq!(res, 900);
     }
 }
