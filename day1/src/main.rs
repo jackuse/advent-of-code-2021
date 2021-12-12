f0ae6f79739fc9268850dff6d2288b073c7d1e9a
Etienne Grandier-Vazeille <etienne.grandiervazeille@gmail.com>
2021-12-12 13:07:41 +0100

    feat(day1): add part 2 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,30 +3,60 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     let filename = "src/input.txt";
-    let res = part_1_count_increase(read_file(filename));
+    let number_list = convert_string_to_number(read_file(filename));
 
-    println!("Number of increase : {}", res);
+    // number_list.iter().for_each(|w| println!("{}", *w));
+
+    let res = part_1_count_increase(&number_list);
+    println!("PART 1 : Number of increase : {}", res);
+
+    let res = part_2(&number_list);
+    println!("PART 2 : Number of increase : {}", res);
 }
 
-fn part_1_count_increase(readings: Vec<String>) -> i32 {
-    let number_list = readings
-        .into_iter()
-        .map(|s| s.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
-
+fn part_1_count_increase(number_list: &Vec<i32>) -> i32 {
     let mut acc = 0;
     let mut last_val = number_list[0];
     for (pos, val) in number_list.into_iter().enumerate() {
         if pos == 0 {
             continue;
         }
-        if val > last_val {
+        if *val > last_val {
             acc += 1;
         }
-        last_val = val
+        last_val = *val
     }
 
     return acc;
+}
+
+fn part_2(number_list: &Vec<i32>) -> i32 {
+    let mut window_list: Vec<i32> = vec![];
+    for (pos, val) in number_list.into_iter().enumerate() {
+        window_list.push(*val);
+        if pos as i32 - 1 >= 0 {
+            window_list[pos - 1] = val + window_list[pos - 1];
+        }
+        if pos as i32 - 2 >= 0 {
+            window_list[pos - 2] = val + window_list[pos - 2];
+        }
+    }
+
+    // remove last 2 incomplete values
+    window_list.pop();
+    window_list.pop();
+
+    // println!("window");
+    // window_list.iter().for_each(|w| println!("{}", *w));
+
+    part_1_count_increase(&window_list)
+}
+
+fn convert_string_to_number(vector: Vec<String>) -> Vec<i32> {
+    vector
+        .into_iter()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
 }
 
 fn read_file(filename: &str) -> Vec<String> {
@@ -72,8 +102,18 @@ mod tests {
     #[test]
     fn should_pass_part1_demo() {
         let filename = "src/report.txt";
-        let res = part_1_count_increase(read_file(filename));
+        let number_list = convert_string_to_number(read_file(filename));
+        let res = part_1_count_increase(&number_list);
 
         assert_eq!(res, 7);
+    }
+
+    #[test]
+    fn should_pass_part2_demo() {
+        let filename = "src/report.txt";
+        let number_list = convert_string_to_number(read_file(filename));
+        let res = part_2(&number_list);
+
+        assert_eq!(res, 5);
     }
 }
