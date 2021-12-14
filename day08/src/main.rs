5f78af34d9d1742009493d9caac52dc4f95adf5b
Etienne Grandier-Vazeille <etienne.grandiervazeille@gmail.com>
2021-12-14 14:08:22 +0100

    feat(day08): add part 2 solution

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,5 +1,3 @@
-use std::io::BufRead;
-
 use shared::read_file;
 
 fn main() {
@@ -33,16 +31,16 @@ fn part1(data: &Vec<String>) -> i32 {
 
     // println!("digit_list \n{:?}", digit_list);
 
-    // 0 => 5
+    // 0 => 6 +
     // 1 => 2 *
     // 2 => 5
     // 3 => 5
     // 4 => 4 *
     // 5 => 5
-    // 6 => 6
+    // 6 => 6 +
     // 7 => 3 *
     // 8 => 7 *
-    // 9 => 6
+    // 9 => 6 +
 
     let easy_by_list: Vec<i32> = digit_list
         .iter()
@@ -67,8 +65,282 @@ fn part1(data: &Vec<String>) -> i32 {
 
     easy_by_list.iter().sum::<i32>()
 }
-fn part2(data: &Vec<String>) -> u32 {
+
+fn part2(data: &Vec<String>) -> usize {
+    let data_and_needle_list: Vec<Vec<Vec<String>>> = data
+        .iter()
+        .map(|line| {
+            line.split("|")
+                .map(|part| part.to_string())
+                .map(|part| {
+                    part.split(" ")
+                        .map(|s| s.to_string())
+                        .filter(|s| (*s).len() > 0) // remove empty string
+                        .map(|s| sort(&s)) // sort A-Z
+                        .collect::<Vec<String>>()
+                })
+                .collect()
+        })
+        .collect();
+
+    let number_found_list: Vec<String> = data_and_needle_list.iter().map(|data_and_needle| {
+        let mut finder = Finder::new();
+
+        finder.init(&data_and_needle[0]);
+        finder.deduce_other(&data_and_needle[0]);
+        finder.decode_number(&data_and_needle[1])
+
+    }).collect();
+
+    number_found_list.iter().map(|number| number.parse::<usize>().unwrap()).sum()
+}
+
+fn _part2_old(data: &Vec<String>) -> usize {
+    let data_and_needle: Vec<Vec<Vec<String>>> = data
+        .iter()
+        .map(|line| {
+            line.split("|")
+                .map(|part| part.to_string())
+                .map(|part| {
+                    part.split(" ")
+                        .map(|s| s.to_string())
+                        .filter(|s| (*s).len() > 0) // remove empty string
+                        .map(|s| sort(&s)) // sort A-Z
+                        .collect::<Vec<String>>()
+                })
+                .collect()
+        })
+        .collect();
+
+    // println!("data_and_needle \n{:?}", &data_and_needle);
+
+    let data_clean = &data_and_needle[0][0];
+    // println!("data_clean \n{:?}", data_clean);
+
+    //  aaaa
+    // b    c
+    // b    c
+    //  dddd
+    // e    f
+    // e    f
+    //  gggg
+    //
+    // Depuis les unique 1 4 7 8
+    // manque grp 6 9 0  (t6) et grp 2 3 5 (t5)
+    //
+    // t2 => cf
+    // t6 sans c ou f => 6
+    // ========= 1 4 7 8 6 RAF 9 0 2 3 5
+    // t6 avec 4 => 0 => 0 et 9 trouvé
+    // ========= 1 4 7 8 6 9 0 RAF 2 3 5
+    // t5 avec 1 => 3
+    // 9 sans 6 => C trouvé
+    // t5 sans C => 5 => 2 et  trouvé
+
+    let mut memory = Finder::new();
+
+    // Init with know value
+    data_clean.iter().for_each(|digit| {
+        if digit.len() == 2 {
+            memory.save(1, &digit);
+        } else if digit.len() == 3 {
+            memory.save(7, &digit);
+        } else if digit.len() == 4 {
+            memory.save(4, &digit);
+        } else if digit.len() == 7 {
+            memory.save(8, &digit);
+        }
+    });
+
+    println!("{:?}", &memory);
+
+    // Find 6 9 0 3
+    data_clean.iter().for_each(|digit| {
+        if digit.len() == 6 {
+            let decomp_one = memory.get_decomposed(1);
+
+            if !digit.contains(decomp_one[0]) || !digit.contains(decomp_one[1]) {
+                memory.save(6, &digit);
+            } else {
+                let is_0 = contain_needles(&memory.get_decomposed(4), &digit);
+
+                if is_0 {
+                    memory.save(9, &digit);
+                } else {
+                    memory.save(0, &digit);
+                }
+            }
+        } else if digit.len() == 5 {
+            let is_3 = contain_needles(&memory.get_decomposed(1), &digit);
+
+            if is_3 {
+                memory.save(3, &digit);
+            }
+        }
+    });
+
+    // find 5 2
+    data_clean.iter().for_each(|digit| {
+        if digit.len() == 5 {
+            let mut segment_c = "";
+            memory.get_decomposed(9).iter().for_each(|&letter| {
+                if !contain_needles(&vec![letter], &memory.get(6)) {
+                    segment_c = letter;
+                }
+            });
+            // println!("segment_C {:?}", &segment_c);
+
+            if not_contain_needles(&vec![segment_c], digit) && *digit != memory.get(3) {
+                memory.save(5, &digit);
+            } else if *digit != memory.get(3) {
+                memory.save(2, &digit);
+            }
+        }
+    });
+
+    println!("{:?}", &memory);
+
+    let number_clean = &data_and_needle[0][1];
+
+    let number_found: Vec<usize> = number_clean.iter().map(|a| memory.get_number(a)).collect();
+
+    println!("number_clean {:?}", &number_clean);
+    println!("number_found {:?}", &number_found);
+
+    let res = number_found
+        .iter()
+        .map(|v| v.to_string())
+        .collect::<Vec<String>>()
+        .join("");
+
+    println!("res {:?}", &res);
+
     0
+}
+
+fn contain_needles(needles: &Vec<&str>, haystack: &String) -> bool {
+    needles
+        .iter()
+        .map(|&letter| haystack.contains(letter))
+        .fold(true, |acc, x| acc && x)
+}
+
+fn not_contain_needles(needles: &Vec<&str>, haystack: &String) -> bool {
+    needles
+        .iter()
+        .map(|&letter| !haystack.contains(letter))
+        .fold(true, |acc, x| acc && x)
+}
+
+fn sort(string: &String) -> String {
+    let mut chars: Vec<char> = string.chars().collect();
+    chars.sort_unstable();
+    chars.into_iter().collect::<String>()
+}
+
+#[derive(Debug)]
+struct Finder {
+    memory: Vec<String>,
+}
+
+impl Finder {
+    pub fn new() -> Self {
+        Self {
+            memory: vec!["".to_string(); 10],
+        }
+    }
+
+    pub fn init(&mut self, data: &Vec<String>) {
+        // Init with know value
+        data.iter().for_each(|digit| {
+            if digit.len() == 2 {
+                self.save(1, &digit);
+            } else if digit.len() == 3 {
+                self.save(7, &digit);
+            } else if digit.len() == 4 {
+                self.save(4, &digit);
+            } else if digit.len() == 7 {
+                self.save(8, &digit);
+            }
+        });
+    }
+
+    pub fn deduce_other(&mut self, data: &Vec<String>) {
+        // Find 6 9 0 3
+        data.iter().for_each(|digit| {
+            if digit.len() == 6 {
+                let decomp_one = self.get_decomposed(1);
+
+                if !digit.contains(decomp_one[0]) || !digit.contains(decomp_one[1]) {
+                    self.save(6, &digit);
+                } else {
+                    let is_0 = contain_needles(&self.get_decomposed(4), &digit);
+
+                    if is_0 {
+                        self.save(9, &digit);
+                    } else {
+                        self.save(0, &digit);
+                    }
+                }
+            } else if digit.len() == 5 {
+                let is_3 = contain_needles(&self.get_decomposed(1), &digit);
+
+                if is_3 {
+                    self.save(3, &digit);
+                }
+            }
+        });
+
+        // find 5 2
+        data.iter().for_each(|digit| {
+            if digit.len() == 5 {
+                let mut segment_c = "";
+                self.get_decomposed(9).iter().for_each(|&letter| {
+                    if !contain_needles(&vec![letter], &self.get(6)) {
+                        segment_c = letter;
+                    }
+                });
+                // println!("segment_C {:?}", &segment_c);
+
+                if not_contain_needles(&vec![segment_c], digit) && *digit != self.get(3) {
+                    self.save(5, &digit);
+                } else if *digit != self.get(3) {
+                    self.save(2, &digit);
+                }
+            }
+        });
+    }
+
+    pub fn decode_number(&self, segment_number: &Vec<String>) -> String {
+        let number_found: Vec<usize> = segment_number.iter().map(|a| self.get_number(a)).collect();
+        number_found
+            .iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>()
+            .join("")
+    }
+
+    pub fn save(&mut self, number: usize, string: &String) {
+        self.memory[number] = string.to_string();
+    }
+
+    pub fn get(&self, number: usize) -> String {
+        self.memory[number].to_string()
+    }
+
+    pub fn get_decomposed(&self, number: usize) -> Vec<&str> {
+        self.memory[number]
+            .split("")
+            .filter(|s| (*s).len() > 0)
+            .collect()
+    }
+    pub fn get_number(&self, string: &String) -> usize {
+        // println!("{:?}", string);
+        self.memory
+            .iter()
+            .position(|a| a.as_str() == string.as_str())
+            .unwrap()
+    }
 }
 
 #[cfg(test)]
